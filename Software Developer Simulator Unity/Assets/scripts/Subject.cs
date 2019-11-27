10f7603f6300a7c06b89b10e47199123cfa34078
Surya Jasper <suryajasper2004@gmail.com>
2019-11-26 16:36:56 -0800

    Added a way to manage class grades
    modified the subject class so it now holds grades and contains methods for getting informationa bout them

--- Software Developer Simulator Unity/Assets/scripts/Subject.cs
+++ Software Developer Simulator Unity/Assets/scripts/Subject.cs
@@ -9,4 +9,94 @@ using UnityEngine;
     [Range(0f, 1f)] public float chanceOfPopQuiz;
     [Range(0,7)] public int testFrequency;
     [Range(0, 7)] public int essayFrequency;
+    public struct Grade {
+        public string assignmentTitle;
+        public float possiblePoints;
+        public float earnedPoints;
+
+        public Grade(string aT, float pP, float eP)
+        {
+            assignmentTitle = aT;
+            possiblePoints = pP;
+            earnedPoints = eP;
+        }
+    }
+
+    private Dictionary<string, List<Grade>> gradeDict;
+
+    public Subject(string name)
+    {
+        gradeDict = new Dictionary<string, List<Grade>>();
+        this.name = name;
+    }
+    public void addGrade(string category, string title, string grade)
+    {
+        gradeDict[category].Add(new Grade(title, float.Parse(grade.Split('/')[0]), float.Parse(grade.Split('/')[1])));
+    }
+
+    public string getCatGradeAsString(string category)
+    {
+        List<float> received = new List<float>();
+        List<float> possible = new List<float>();
+
+        if (gradeDict.ContainsKey(category))
+        {
+            foreach (Grade grade in gradeDict[category])
+            {
+                received.Add(grade.earnedPoints);
+                possible.Add(grade.possiblePoints);
+            }
+            float possibleSum = 0f;
+            float receivedSum = 0f;
+            for (int i =0; i < received.Count; i++)
+            {
+                possibleSum += possible[i];
+                receivedSum += received[i];
+            }
+            if (System.Math.Abs(possibleSum) < 0.5f)
+                return null;
+            return System.Math.Round(possibleSum, 1, System.MidpointRounding.ToEven).ToString() + "/" + System.Math.Round(receivedSum, 1, System.MidpointRounding.ToEven).ToString();
+        } 
+        return null;
+    }
+    public float getCatGrade(string category)
+    {
+        string str = getCatGradeAsString(category);
+        if (str == null) return -1f;
+        return float.Parse(str.Split('/')[0]) / float.Parse(str.Split('/')[1]);
+    }
+    public string getAssignmentGradeAsString(string assignmentName)
+    {
+        foreach (List<Grade> grades in gradeDict.Values)
+            foreach (Grade grade in grades)
+                if (grade.assignmentTitle == assignmentName)
+                    return System.Math.Round(grade.earnedPoints, 1, System.MidpointRounding.ToEven).ToString() + "/" + System.Math.Round(grade.possiblePoints, 1, System.MidpointRounding.ToEven).ToString();
+        return null;
+    }
+    public float getAssignmentGrade(string assignmentName)
+    {
+        string str = getAssignmentGradeAsString(assignmentName);
+        if (str == null) return -1f;
+        return float.Parse(str.Split('/')[0]) / float.Parse(str.Split('/')[1]);
+    }
+    public string getGradeAsString()
+    {
+        float possible = 0f;
+        float received = 0f;
+        foreach(string cat in gradeDict.Keys)
+        {
+            string[] catOut = getCatGradeAsString(cat).Split('/');
+            possible += float.Parse(catOut[1]);
+            received += float.Parse(catOut[0]);
+        }
+        if (System.Math.Abs(possible) < 0.5f)
+            return null;
+        return System.Math.Round(received, 1, System.MidpointRounding.ToEven).ToString() + "/" + System.Math.Round(possible, 1, System.MidpointRounding.ToEven).ToString();
+    }
+    public float getGrade()
+    {
+        string str = getGradeAsString();
+        if (str == null) return -1f;
+        return float.Parse(str.Split('/')[0]) / float.Parse(str.Split('/')[1]);
+    }
 }
